87e0d5d92464993b20b67026a8935ee3ee87ea82
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-05-05 02:56:13 +0800

    Finishing CSS

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,7 @@ import "../App.css";
 export default function NavBar() {
   return (
     <div className="Footer">
-      <div></div>
-      <div>Created By: Thomas Maynard</div>
+      <div className="CreatedBy">Created By: Thomas Maynard</div>
     </div>
   );
 }
